864b401104c0167e976f693660b1d786aa770286
Vicente Hoare Silva <vicentehoares@hotmail.com>
2021-02-15 22:14:53 +0000

    Se arreglaron sintax errors y agrego static_dir, ademas de carpetas para media y static

--- sgd_grmd/settings.py
+++ sgd_grmd/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'template'
-STATIC_DIR = BASE_DIR / 'static'
+STATIC_DIR = BASE_DIR / 'static_dir'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -135,7 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STARICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
